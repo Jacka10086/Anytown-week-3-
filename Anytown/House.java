652b8ede9155fc13106fdf931ddf65adafd3b53b
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-13 19:13:22 +0100

    Update House.java

--- Anytown/House.java
+++ Anytown/House.java
@@ -2,18 +2,19 @@ package Anytown;
 
 public class House extends Building {
 
-    private int numBedrooms;
-    private boolean hasGarage;
-    private static int numHouses = 0;
+    private int numBedrooms; // 卧室数量
+    private boolean hasGarage; // 是否有车库
+    private static int numHouses = 0; // 房屋数量
 
+    // 无参构造函数
     public House() {
         super();
         this.numBedrooms = 0;
         this.hasGarage = false;
         House.numHouses++;
-
     }
     
+    // 带参构造函数
     public House (String address, String owner, int numBedrooms, boolean hasGarage) {
         super(address, owner);
         this.numBedrooms = numBedrooms;
@@ -21,29 +22,23 @@ public class House extends Building {
         House.numHouses++;
     }
 
+    // 获取卧室数量
     public int getNumBedrooms() {
         return this.numBedrooms;
     }
 
+    // 设置卧室数量
     public void setNumBedrooms(int numBedrooms) {
         this.numBedrooms = numBedrooms;
     }
 
+    // 获取是否有车库
     public boolean getHasGarage() {
         return this.hasGarage;
     }
 
+    // 设置是否有车库
     public void setHasGarage(boolean hasGarage) {
         this.hasGarage = hasGarage;
     }
-
-    public String toString() {
-        return super.toString() + " and is a house with " + 
-               this.numBedrooms + " bedrooms. ";
-    }
-
-    public static int getNumHouses() {
-        return House.numHouses;
-    }
 }
-
