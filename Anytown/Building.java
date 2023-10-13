bbf9354ba8d0d63ce1ae5c8392c0f7f508f92c28
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-13 19:21:15 +0100

    Update Building.java

--- Anytown/Building.java
+++ Anytown/Building.java
@@ -1,63 +1,44 @@
 package Anytown;
 
-public class Building {
-    
-    private String address;
-    private String owner;
-    private static int numBuildings = 0;
+public class House extends Building {
 
-    public Building() {
-        this.address = "";
-        this.owner = "";
-        Building.numBuildings++;
+    private int numBedrooms; // 卧室数量
+    private boolean hasGarage; // 是否有车库
+    private static int numHouses = 0; // 房屋数量
+
+    // 无参构造函数
+    public House() {
+        super();
+        this.numBedrooms = 0;
+        this.hasGarage = false;
+        House.numHouses++;
+    }
+    
+    // 带参构造函数
+    public House (String address, String owner, int numBedrooms, boolean hasGarage) {
+        super(address, owner);
+        this.numBedrooms = numBedrooms;
+        this.hasGarage = hasGarage;
+        House.numHouses++;
     }
 
-public Building(String address, String owner) {
-    this.address = address;
-    this.owner = owner;
-    Building.numBuildings++; 
+    // 获取卧室数量
+    public int getNumBedrooms() {
+        return this.numBedrooms;
+    }
 
+    // 设置卧室数量
+    public void setNumBedrooms(int numBedrooms) {
+        this.numBedrooms = numBedrooms;
+    }
+
+    // 获取是否有车库
+    public boolean getHasGarage() {
+        return this.hasGarage;
+    }
+
+    // 设置是否有车库
+    public void setHasGarage(boolean hasGarage) {
+        this.hasGarage = hasGarage;
+    }
 }
-
-public String getAddress() {
-    return this.address;
-}
-
-public void setAddress(String address) {
-    this.address = address;
-}
-
-public String getOwner() {
-    return this.owner;
-}
-
-public void setOwner(String owner) {
-    this.owner = owner;
-}
-
-public String toString() {
-    return this.address + " occupied by " + this.owner;
-}
-
-public static int getNumBuildings() {
-    return Building.numBuildings; 
-}
-
-public boolean equals(Building anotherBuilding) {
-    return this.getAddress().equals(anotherBuilding.getAddress());
-}
-
-public static void main(String[] args) {
-    Building b1 = new Building();
-    Building b2 = new Building("27 Main Street", "Rex Dog Groomer");
-    Building b3 = new Building("18 Low Street", "Mary Jones");
-
-    b1.setAddress("3 High Street");
-    b1.setOwner("Smith's Newsagent");
-
-    System.out.println(b1);
-    System.out.println(b2);
-    System.out.println(b3);
-}
-}
-
