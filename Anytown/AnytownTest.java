df03cb7f72dafed6d9c7fd8ac5dcdfbd90842907
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-14 01:33:46 +0100

    Update AnytownTest.java

--- Anytown/AnytownTest.java
+++ Anytown/AnytownTest.java
@@ -6,7 +6,10 @@ import java.io.File;
 public class AnytownTest {
     public static void main(String[] args) throws Exception {
 
+        // 创建一个容量为 13 的 ArrayBag 对象
         ArrayBag buildings = new ArrayBag(13);
+
+        // 从文件中读取数据
         File fileIn = new File("Anytown/buildings.txt");
         Scanner fileInput = new Scanner(fileIn);
         String buildingData;
@@ -16,8 +19,11 @@ public class AnytownTest {
         boolean hasGarage;
         
 
+        // 逐行读取文件中的数据
         while (fileInput.hasNextLine()) {
             buildingData = fileInput.nextLine();
+
+            // 解析数据
             marker2 = buildingData.indexOf("|", marker1);
             marker3 = buildingData.indexOf("|", marker2 + 1);
             marker4 = buildingData.indexOf("|", marker3 + 1);
@@ -26,7 +32,8 @@ public class AnytownTest {
             address = buildingData.substring(marker1, marker2);
             owner = buildingData.substring(marker2 + 1, marker3);
 
-             switch (buildingData.substring(0,1)) {
+            // 根据数据类型创建相应的对象，并将其添加到 ArrayBag 中
+        switch (buildingData.substring(0,1)) {
                 case "B" :
                 System.out.println("Building");
                 if (buildings.addNewEntry(new Building(address, owner))) {
