44b1858d28bbfe657f217259a6dcd6a185b499f5
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-14 15:42:10 +0100

    Add files via upload

---
+++ BagInterface.java
@@ -0,0 +1,27 @@
+package Anytown;
+
+public interface BagInterface {
+
+    public int getCurrentSize();
+
+    public boolean isEmpty();
+
+    public boolean addNewEntry(Building newEntry);
+
+    public Building remove();
+
+    public boolean remove(Building anEntry);
+
+    public void clear();    
+
+    public int getFrequencyOf(Building anEntry);
+
+    public boolean contains(Building anEntry);
+
+    public String toString();
+
+
+
+
+    
+}
